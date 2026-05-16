0ad432c8e89d63d2e63868da5858d46ecb69815a
natalyjohanaardoncanales-ops <natalyjohanaardoncanales@gmail.com>
2026-05-16 10:48:16 -0600

    primer commit demojpa

---
+++ src/test/java/com/demojpa1/Demojpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demojpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demojpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
