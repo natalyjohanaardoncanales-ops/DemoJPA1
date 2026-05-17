4bcc3e1d0cd81a23e328990b39a6c626b1f64af8
natalyjohanaardoncanales-ops <natalyjohanaardoncanales@gmail.com>
2026-05-17 08:22:48 -0600

    complemento de los metodos

--- src/main/java/com/demojpa1/repository/ICategoriaRepository.java
+++ src/main/java/com/demojpa1/repository/ICategoriaRepository.java
@@ -1,11 +1,10 @@
 package com.demojpa1.repository;
 
 
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.demojpa1.models.Categoria;
 
-public interface ICategoriaRepository extends CrudRepository<Categoria, Integer> {
+public interface ICategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 }
